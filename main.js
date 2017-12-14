4dd28a93aa9909f643419392c3dc8f8ec478aa8b
ItaySherMaN <itay2001@gmail.com>
2017-12-15 01:52:25 +0200

    added file utils.js, styled the index.html and cleared the main.js temp code

--- main.js
+++ main.js
@@ -2,9 +2,3 @@ canvas = document.getElementById('canvas')
 g = canvas.getContext('2d')
 width = canvas.width
 height = canvas.height
-
-g.fillStyle = 'violet'
-g.fillRect(0, 0, width, height)
-
-g.fillStyle = 'blue'
-g.fillRect(width / 2, height / 3, width, height)
---
+++ utils.js
@@ -0,0 +1,3 @@
+const index = (row, col) => row * 8 + col
+const row = index => index / 8
+const col = index => index % 8
