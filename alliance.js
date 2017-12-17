882973e01cd2fcb835185366b1826450ab003388
ItaySherMaN <itay2001@gmail.com>
2017-12-17 23:46:32 +0200

    added a direction and a starting row for each alliance

--- alliance.js
+++ alliance.js
@@ -1,6 +1,14 @@
 const Alliance = {
 	WHITE: 'w',
-	BLACK: 'b'
+	BLACK: 'b',
+
+	direction: function(alliance) {
+		return alliance === this.WHITE ? 1 : -1
+	}
+
+	startingRow: function(alliance) {
+		return alliance === this.WHITE ? 1 : 6
+	}
 }
 
 module.exports.Alliance = Alliance
