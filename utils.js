d5e8864852cbb3dfd93220a0a7eed0af28ec7187
ItaySherMaN <itay2001@gmail.com>
2017-12-17 22:10:03 +0200

    added more functions in utils.js, more specifically started working with coordinates too rather than just with the board index

--- utils.js
+++ utils.js
@@ -2,9 +2,21 @@ const boardjs = require('./board/board')
 
 const Board = boardjs.Board
 
-module.exports = {
-	index: (row, col) => row * Board.COLS + col,
-	row: index => index / Board.COLS,
-	col: index => index % Board.ROWS,
-	isValidPosition: index => index >= 0 && index < Board.NUM_TILES
+const indexFromCoordinates = (row, col) => row * Board.COLS + col
+const row = index => parseInt(index / Board.COLS)
+const col = index => index % Board.ROWS
+const isValidIndex = index => index >= 0 && index < Board.NUM_TILES
+const areValidCoordinates = (row, col) => {
+	return row >= 0 && row < Board.ROWS
+		&& col >= 0 && col < Board.COLS
 }
+const indexFromOffset = (index, offset) => {
+	return indexFromCoordinates(row(index) + row(offset), col(index) + col(offset))
+}
+
+module.exports.indexFromCoordinates = indexFromCoordinates
+module.exports.row = row
+module.exports.col = col
+module.exports.isValidIndex = isValidIndex
+module.exports.areValidCoordinates = areValidCoordinates
+module.exports.indexFromOffset = indexFromOffset
