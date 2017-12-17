5e739ba4d28cdb175abb70d16864a7141abceb61
ItaySherMaN <itay2001@gmail.com>
2017-12-17 22:15:55 +0200

    piece.js - added a new function that generates the pseudo legal moves for a sliding piece - this includes the bishop, rook and queen, great function!

--- pieces/piece.js
+++ pieces/piece.js
@@ -6,10 +6,49 @@ const Piece = {
 		obj.alliance = alliance
 
 		return obj
-	}
+	},
 
 	// pseudo legal moves ()
-	// legal moves ()
+
+	legalMoves: function(board) {
+		return this.pseudoLegalMoves().filter(move => {
+			return !board.play(move).isCheck()
+		})
+	}
+}
+
+const slidingPiecePseudoLegalMoves = function(board, movedPiece) {
+	const moves = []
+
+	movedPiece.moveOffsets.forEach(offset => {
+		let destRow = utilsjs.row(movedPiece.index) + offset.rowOffset
+		let destCol = utilsjs.col(movedPiece.index) + offset.colOffset
+		let destIndex = utilsjs.indexFromCoordinates(destRow, destCol)
+
+		let facedAPiece = false
+
+		while (utilsjs.areValidCoordinates(destRow, destCol) && !facedAPiece) {
+			const destTile = board.get(destIndex)
+
+			if (destTile.empty) {
+				moves.push(RegularMove.create(board, destIndex, movedPiece))
+			}
+			else {
+				const destPiece = destTile.piece
+
+				if (destPiece.alliance !== movedPiece.alliance) {
+					moves.push(AttackingMove.create(board, destIndex, movedPiece, destPiece))
+				}
+				facedAPiece = true
+			}
+
+			destRow += offset.rowOffset
+			destCol += offset.colOffset
+			destIndex = utilsjs.indexFromCoordinates(destRow, destCol)
+		}
+	})
+
+	return moves
 }
 
 module.exports.Piece = Piece
