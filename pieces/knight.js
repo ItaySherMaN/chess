acdcd85eb9cb1d820a0a5cf6628e17f7f40cf7ab
ItaySherMaN <itay2001@gmail.com>
2017-12-17 22:14:35 +0200

    modified the knight class so it will be using the new coordinate checker and not just the tile index

--- pieces/knight.js
+++ pieces/knight.js
@@ -1,6 +1,7 @@
 const piecejs = require('./piece')
 const typejs = require('./../type')
 const movejs = require('./../board/move')
+const utilsjs = require('./..utils')
 
 const Type = typejs.Type
 const Piece = piecejs.Piece
@@ -8,7 +9,16 @@ const RegularMove = movejs.RegularMove
 const AttackingMove = movejs.AttackingMove
 
 const Knight = {
-	moveOffsets: [-17, -15, -10, -6, 6, 10, 15, 17],
+	moveOffsets: [
+		{rowOffset: 2, colOffset: 1},
+		{rowOffset: 2, colOffset: -1},
+		{rowOffset: 1, colOffset: 2},
+		{rowOffset: 1, colOffset: -2},
+		{rowOffset: -1, colOffset: 2},
+		{rowOffset: -1, colOffest: -2},
+		{rowOffset: -2, colOffset: 1},
+		{rowOffset: -2, colOffset: -1}
+	],
 
 	create: function(index, alliance) {
 		const obj = Piece.create(index, alliance)
@@ -17,69 +27,42 @@ const Knight = {
 	}
 
 	pseudoLegalMoves: function(board) {
-		let destIndex
-		let destTile
-
 		const moves = []
 
 		Knight.moveOffsets.forEach(offset => {
-			destIndex = this.index + offset
+			const destRow = utilsjs.row(this.index) + offset.rowOffset
+			const destCol = utilsjs.col(this.index) + offset.colOffset
+			const destIndex = utilsjs.indexFromCoordinates(destRow, destCol)
 
-			if (utils.isValidPosition(destIndex)) {
-				if (isValidOffset(offset, this.index)) {
-					destTile = board.get(destIndex)
-					destPiece = destTile.piece
+			if (utilsjs.areValidCoordinates(destRow, destCol)) {
+				const destTile = board.get(destIndex)
 
-					if (destTile.empty) {
-						moves.push(RegularMove.create(board, destIndex, this))
-					}
-					else if (destPiece.alliance != this.alliance) {
+				if (destTile.empty) {
+					moves.push(RegularMove.create(board, destIndex, this))
+				}
+				else {
+					const destPiece = destTile.piece
+
+					if (destPiece.alliance !== this.alliance) {
 						moves.push(AttackingMove.create(board, destIndex, this, destPiece))
 					}
 				}
 			}
 		})
-	},
-
-	legalMoves: function(board) {
-		return this.pseudoLegalMoves().filter(move => {
-			return !board.play(move).isCheck()
-		})
 	}
-}
-
-const isValidOffset = function(offset, index) {
-	const col = utils.col(index)
-
-	if (col < 2) {
-		if (col === 0) {
-			return !in(offset, [-17, -10, 6, 15])
-		}
-		return !in(offset, [-10, 6])
-	}
-	else if (col > 5) {
-		if (col === 7) {
-			return !in(offset, [-15, -6, 10, 17])
-		}
-		return !in(offset, [-6, 10])
-	}
-}
-
-const in = function(value, array) {
-	array.forEach(arrayValue => {
-		if (value === arrayValue) {
-			return true
-		}
-	})
-
-	return false
 }
 
 module.exports.Knight = Knight
 
+/*
 
-//--X-X---
-//-X---X--
-//---o----
-//-X---X--
-//--X-X---
+--------
+--X-X---
+-X---X--
+---o----
+-X---X--
+--X-X---
+--------
+--------
+
+*/
