94e9f685c763bf3e4f4056f3e13b382a0c26db76
ItaySherMaN <itay2001@gmail.com>
2017-12-17 13:21:50 +0200

    minor change in the way the exports were handled

--- pieces/piece.js
+++ pieces/piece.js
@@ -1,18 +1,15 @@
-const typejs = require('./../type')
+const Piece = {
+	create: function(index, alliance) {
+		const obj = Object.create(this)
 
-module.exports = {
-	Type: typejs.Type,
-	Piece: {
-		create: function(index, alliance) {
-			const obj = Object.create(this)
+		obj.index = index
+		obj.alliance = alliance
 
-			obj.index = index
-			obj.alliance = alliance
-
-			return obj
-		}
-
-		// pseudo legal moves ()
-		// legal moves ()
+		return obj
 	}
+
+	// pseudo legal moves ()
+	// legal moves ()
 }
+
+module.exports.Piece = Piece
