9ddb45e6f1e8ad541ce3922643df6f0105bafbcf
ItaySherMaN <itay2001@gmail.com>
2017-12-17 22:11:33 +0200

    minor change in board class

--- board/board.js
+++ board/board.js
@@ -1,16 +1,22 @@
+const alliancejs = require('./../alliance')
+
+const Alliance = alliancejs.Alliance
+
+const generateNewTileBoard = function() {
+	return []
+}
+
 const Board = {
 	ROWS: 8,
 	COLS: 8,
 	NUM_TILES: 64,
 
-	turn: Alliance.WHITE,
 	tiles: generateNewTileBoard(),
+	turn: Alliance.WHITE,
 
 	create: function() {
 		const obj = Object.create(this)
 
-		obj.rows = Board.ROWS
-		obj.col = Board.COLS
 		obj.tiles = this.tiles
 		obj.turn = this.turn
 
@@ -24,10 +30,6 @@ const Board = {
 
 		return newBoard
 	}
-}
-
-const generateNewTileBoard = function() {
-	return []
 }
 
 module.exports.Board = Board
