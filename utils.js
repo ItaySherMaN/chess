8fcb5111d50556234cf013e2b68fb47383094b6a
ItaySherMaN <itay2001@gmail.com>
2017-12-18 02:01:52 +0200

    cleaned the utils.js file from a lot of junk that wasn't needed

--- utils.js
+++ utils.js
@@ -2,16 +2,11 @@ const boardjs = require('./board/board')
 
 const Board = boardjs.Board
 
-const indexFromCoordinates = (row, col) => row * Board.COLS + col
-const row = index => parseInt(index / Board.COLS)
-const col = index => index % Board.ROWS
-const isValidIndex = index => index >= 0 && index < Board.NUM_TILES
+const index = (row, col) => row * Board.COLS + col
 const areValidCoordinates = (row, col) => {
 	return row >= 0 && row < Board.ROWS
 		&& col >= 0 && col < Board.COLS
 }
 
-module.exports.indexFromCoordinates = indexFromCoordinates
-module.exports.row = row
-module.exports.col = col
+module.exports.index = index
 module.exports.areValidCoordinates = areValidCoordinates
